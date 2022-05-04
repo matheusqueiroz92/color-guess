fc6b8c4e9ba4eb460a92522b2a2cfe5bacf0f1ad
Matheus Queiroz <matheus_giga@hotmail.com>
2022-05-04 20:31:36 -0300

    Finalizando ultimo requisito

--- script.js
+++ script.js
@@ -16,9 +16,16 @@ window.onload = function colorGuess () {
     divFrase.appendChild(frase);
 
     let arrayColors = [];
+    let placar = 0;
+    let contadorClick = 0;
+
+    let containerScore = document.getElementById('container-score');
+    let score = document.createElement('p');
+    score.id = 'score';
+    score.innerText = 'Placar: '+ placar;
+    containerScore.appendChild(score);
 
     function gerar_cor1(opacidade1 = 1) {
-
         let r1 = parseInt(Math.random() * 255);
         let g1 = parseInt(Math.random() * 255);
         let b1 = parseInt(Math.random() * 255);
@@ -30,7 +37,6 @@ window.onload = function colorGuess () {
     console.log(gerar_cor1());
 
     function gerar_cor2(opacidade2 = 1) {
-
         let r2 = parseInt(Math.random() * 255);
         let g2 = parseInt(Math.random() * 255);
         let b2 = parseInt(Math.random() * 255);
@@ -42,7 +48,6 @@ window.onload = function colorGuess () {
     console.log(gerar_cor2());
 
     function gerar_cor3(opacidade3 = 1) {
-
         let r3 = parseInt(Math.random() * 255);
         let g3 = parseInt(Math.random() * 255);
         let b3 = parseInt(Math.random() * 255);
@@ -54,7 +59,6 @@ window.onload = function colorGuess () {
     console.log(gerar_cor3());
 
     function gerar_cor4(opacidade4 = 1) {
-
         let r4 = parseInt(Math.random() * 255);
         let g4 = parseInt(Math.random() * 255);
         let b4 = parseInt(Math.random() * 255);
@@ -66,7 +70,6 @@ window.onload = function colorGuess () {
     console.log(gerar_cor4());
 
     function gerar_cor5(opacidade5 = 1) {
-
         let r5 = parseInt(Math.random() * 255);
         let g5 = parseInt(Math.random() * 255);
         let b5 = parseInt(Math.random() * 255);
@@ -78,7 +81,6 @@ window.onload = function colorGuess () {
     console.log(gerar_cor5());
 
     function gerar_cor6(opacidade6 = 1) {
-
         let r6 = parseInt(Math.random() * 255);
         let g6 = parseInt(Math.random() * 255);
         let b6 = parseInt(Math.random() * 255);
@@ -89,80 +91,151 @@ window.onload = function colorGuess () {
     }
     console.log(gerar_cor6());
 
-    console.log(arrayColors);
-
     let containerRgb = document.getElementById('container-rgb');
     let rgbCode = document.createElement('p');
     rgbCode.id = 'rgb-color';
-    let colorSecret = arrayColors[Math.floor(Math.random() * arrayColors.length)];
+    colorSecret = arrayColors[Math.floor(Math.random() * arrayColors.length)];
     rgbCode.innerText = colorSecret;
     containerRgb.appendChild(rgbCode);
 
-    console.log(colorSecret);
+    console.log(colorSecret + 'secreta');
 
-    cor1.addEventListener('click', function(){
-        if (arrayColors[0] == colorSecret) {
-            frase.style.color = 'blue';
-            frase.innerText = 'Acertou!';
-        }
-        else {
-            frase.style.color = 'red';
-            frase.innerText = 'Errou! Tente novamente!';
+    cor1.addEventListener('click', function(e){
+        if (contadorClick == 0) {
+            cor1.style.border = '2px solid blue';
+            cor2.style.border = '';
+            cor3.style.border = '';
+            cor4.style.border = '';
+            cor5.style.border = '';
+            cor6.style.border = '';
+            contadorClick += 1;
+            console.log(contadorClick);
+            if (arrayColors[0] == colorSecret) {
+                frase.style.color = 'blue';
+                frase.innerText = 'Acertou!';
+                placar += 3;
+                score.innerText = 'Placar: ' + placar;
+            }
+            else {
+                frase.style.color = 'red';
+                frase.innerText = 'Errou! Tente novamente!';
+                cor1.style.border = '2px solid red';
+            }
         }
     });
 
-    cor2.addEventListener('click', function(){
-        if (arrayColors[1] == colorSecret) {
-            frase.style.color = 'blue';
-            frase.innerText = 'Acertou!';
-        }
-        else {
-            frase.style.color = 'red';
-            frase.innerText = 'Errou! Tente novamente!';
-        }
-    });
-
-    cor3.addEventListener('click', function(){
-        if (arrayColors[2] == colorSecret) {
-            frase.style.color = 'blue';
-            frase.innerText = 'Acertou!';
-        }
-        else {
-            frase.style.color = 'red';
-            frase.innerText = 'Errou! Tente novamente!';
-        }
-    });
-
-    cor4.addEventListener('click', function(){
-        if (arrayColors[3] == colorSecret) {
-            frase.style.color = 'blue';
-            frase.innerText = 'Acertou!';
-        }
-        else {
-            frase.style.color = 'red';
-            frase.innerText = 'Errou! Tente novamente!';
+    cor2.addEventListener('click', function(e){
+        if (contadorClick == 0) {
+            cor1.style.border = '';
+            cor2.style.border = '2px solid blue';
+            cor3.style.border = '';
+            cor4.style.border = '';
+            cor5.style.border = '';
+            cor6.style.border = '';
+            contadorClick += 1;
+            if (arrayColors[1] == colorSecret) {
+                frase.style.color = 'blue';
+                frase.innerText = 'Acertou!';
+                placar += 3;
+                score.innerText = 'Placar: ' + placar;
+            }
+            else {
+                frase.style.color = 'red';
+                frase.innerText = 'Errou! Tente novamente!';
+                cor2.style.border = '2px solid red';
+            }
         }
     });
 
-    cor5.addEventListener('click', function(){
-        if (arrayColors[4] == colorSecret) {
-            frase.style.color = 'blue';
-            frase.innerText = 'Acertou!';
-        }
-        else {
-            frase.style.color = 'red';
-            frase.innerText = 'Errou! Tente novamente!';
+    cor3.addEventListener('click', function(e){
+        if (contadorClick == 0) {
+            cor1.style.border = '';
+            cor2.style.border = '';
+            cor3.style.border = '2px solid blue';
+            cor4.style.border = '';
+            cor5.style.border = '';
+            cor6.style.border = '';
+            contadorClick += 1;
+            if (arrayColors[2] == colorSecret) {
+                frase.style.color = 'blue';
+                frase.innerText = 'Acertou!';
+                placar += 3;
+                score.innerText = 'Placar: ' + placar;
+            }
+            else {
+                frase.style.color = 'red';
+                frase.innerText = 'Errou! Tente novamente!';
+                cor3.style.border = '2px solid red';
+            }
         }
     });
 
-    cor6.addEventListener('click', function(){
-        if (arrayColors[5] == colorSecret) {
-            frase.style.color = 'blue';
-            frase.innerText = 'Acertou!';
+    cor4.addEventListener('click', function(e){
+        if (contadorClick == 0) {
+            cor1.style.border = '';
+            cor2.style.border = '';
+            cor3.style.border = '';
+            cor4.style.border = '2px solid blue';
+            cor5.style.border = '';
+            cor6.style.border = '';
+            contadorClick += 1;
+            if (arrayColors[3] == colorSecret) {
+                frase.style.color = 'blue';
+                frase.innerText = 'Acertou!';
+                placar += 3;
+                score.innerText = 'Placar: ' + placar;
+            }
+            else {
+                frase.style.color = 'red';
+                frase.innerText = 'Errou! Tente novamente!';
+                cor4.style.border = '2px solid red';
+            }
         }
-        else {
-            frase.style.color = 'red';
-            frase.innerText = 'Errou! Tente novamente!';
+    });
+
+    cor5.addEventListener('click', function(e){
+        if (contadorClick == 0) {
+            cor1.style.border = '';
+            cor2.style.border = '';
+            cor3.style.border = '';
+            cor4.style.border = '';
+            cor5.style.border = '2px solid blue';
+            cor6.style.border = '';
+            contadorClick += 1;
+            if (arrayColors[4] == colorSecret) {
+                frase.style.color = 'blue';
+                frase.innerText = 'Acertou!';
+                placar += 3;
+                score.innerText = 'Placar: ' + placar;
+            }
+            else {
+                frase.style.color = 'red';
+                frase.innerText = 'Errou! Tente novamente!';
+                cor5.style.border = '2px solid red';
+            }
+        }
+    });
+
+    cor6.addEventListener('click', function(e){
+        if (contadorClick == 0) {
+            cor1.style.border = '';
+            cor2.style.border = '';
+            cor3.style.border = '';
+            cor4.style.border = '';
+            cor5.style.border = '';
+            cor6.style.border = '2px solid blue';
+            contadorClick += 1;
+            if (arrayColors[5] == colorSecret) {
+                frase.style.color = 'blue';
+                frase.innerText = 'Acertou!';
+                placar += 3;
+                score.innerText = 'Placar: ' + placar;
+            }
+            else {
+                frase.style.color = 'red';
+                frase.innerText = 'Errou! Tente novamente!';
+                cor6.style.border = '2px solid red';
+            }
         }
     });
 
@@ -171,7 +244,174 @@ window.onload = function colorGuess () {
     });
 
     btnReset.addEventListener('click', function() {
-        document.location.reload(true);
+        //document.location.reload(true);
+        contadorClick = 0;
+        cor1.style.border = '';
+        cor2.style.border = '';
+        cor3.style.border = '';
+        cor4.style.border = '';
+        cor5.style.border = '';
+        cor6.style.border = '';
+        frase.style.color = 'black';
+        frase.innerText = 'Escolha uma cor';
+        // arrayColors = [];
+        for (let i = arrayColors.length; i > 0; i--) {
+            arrayColors.pop();
+        }
+        colorSecret = '';
+
+        console.log(gerar_cor1());
+        console.log(gerar_cor2());
+        console.log(gerar_cor3());
+        console.log(gerar_cor4());
+        console.log(gerar_cor5());
+        console.log(gerar_cor6());
+
+        colorSecret = arrayColors[Math.floor(Math.random() * arrayColors.length)];
+        rgbCode.innerText = colorSecret;
+
+        cor1.addEventListener('click', function(e){
+            if (contadorClick == 0) {
+                cor1.style.border = '2px solid blue';
+                cor2.style.border = '';
+                cor3.style.border = '';
+                cor4.style.border = '';
+                cor5.style.border = '';
+                cor6.style.border = '';
+                contadorClick += 1;
+                console.log(contadorClick);
+                if (arrayColors[0] == colorSecret) {
+                    frase.style.color = 'blue';
+                    frase.innerText = 'Acertou!';
+                    placar += 3;
+                    score.innerText = 'Placar: ' + placar;
+                }
+                else {
+                    frase.style.color = 'red';
+                    frase.innerText = 'Errou! Tente novamente!';
+                    cor1.style.border = '2px solid red';
+                }
+            }
+        });
+    
+        cor2.addEventListener('click', function(e){
+            if (contadorClick == 0) {
+                cor1.style.border = '';
+                cor2.style.border = '2px solid blue';
+                cor3.style.border = '';
+                cor4.style.border = '';
+                cor5.style.border = '';
+                cor6.style.border = '';
+                contadorClick += 1;
+                if (arrayColors[1] == colorSecret) {
+                    frase.style.color = 'blue';
+                    frase.innerText = 'Acertou!';
+                    placar += 3;
+                    score.innerText = 'Placar: ' + placar;
+                }
+                else {
+                    frase.style.color = 'red';
+                    frase.innerText = 'Errou! Tente novamente!';
+                    cor2.style.border = '2px solid red';
+                }
+            }
+        });
+    
+        cor3.addEventListener('click', function(e){
+            if (contadorClick == 0) {
+                cor1.style.border = '';
+                cor2.style.border = '';
+                cor3.style.border = '2px solid blue';
+                cor4.style.border = '';
+                cor5.style.border = '';
+                cor6.style.border = '';
+                contadorClick += 1;
+                if (arrayColors[2] == colorSecret) {
+                    frase.style.color = 'blue';
+                    frase.innerText = 'Acertou!';
+                    placar += 3;
+                    score.innerText = 'Placar: ' + placar;
+                }
+                else {
+                    frase.style.color = 'red';
+                    frase.innerText = 'Errou! Tente novamente!';
+                    cor3.style.border = '2px solid red';
+                }
+            }
+        });
+    
+        cor4.addEventListener('click', function(e){
+            if (contadorClick == 0) {
+                cor1.style.border = '';
+                cor2.style.border = '';
+                cor3.style.border = '';
+                cor4.style.border = '2px solid blue';
+                cor5.style.border = '';
+                cor6.style.border = '';
+                contadorClick += 1;
+                if (arrayColors[3] == colorSecret) {
+                    frase.style.color = 'blue';
+                    frase.innerText = 'Acertou!';
+                    placar += 3;
+                    score.innerText = 'Placar: ' + placar;
+                }
+                else {
+                    frase.style.color = 'red';
+                    frase.innerText = 'Errou! Tente novamente!';
+                    cor4.style.border = '2px solid red';
+                }
+            }
+        });
+    
+        cor5.addEventListener('click', function(e){
+            if (contadorClick == 0) {
+                cor1.style.border = '';
+                cor2.style.border = '';
+                cor3.style.border = '';
+                cor4.style.border = '';
+                cor5.style.border = '2px solid blue';
+                cor6.style.border = '';
+                contadorClick += 1;
+                if (arrayColors[4] == colorSecret) {
+                    frase.style.color = 'blue';
+                    frase.innerText = 'Acertou!';
+                    placar += 3;
+                    score.innerText = 'Placar: ' + placar;
+                }
+                else {
+                    frase.style.color = 'red';
+                    frase.innerText = 'Errou! Tente novamente!';
+                    cor5.style.border = '2px solid red';
+                }
+            }
+        });
+    
+        cor6.addEventListener('click', function(e){
+            if (contadorClick == 0) {
+                cor1.style.border = '';
+                cor2.style.border = '';
+                cor3.style.border = '';
+                cor4.style.border = '';
+                cor5.style.border = '';
+                cor6.style.border = '2px solid blue';
+                contadorClick += 1;
+                if (arrayColors[5] == colorSecret) {
+                    frase.style.color = 'blue';
+                    frase.innerText = 'Acertou!';
+                    placar += 3;
+                    score.innerText = 'Placar: ' + placar;
+                }
+                else {
+                    frase.style.color = 'red';
+                    frase.innerText = 'Errou! Tente novamente!';
+                    cor6.style.border = '2px solid red';
+                }
+            }
+        });
+
+        console.log(arrayColors);
+        console.log(colorSecret + 'secreta');
+        console.log(rgbCode);
     });
 
 }
